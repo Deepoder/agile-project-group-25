87cd0a3ae7883bb3fd6d609f48868aed69afb3a9
adiulay <45328051+adiulay@users.noreply.github.com>
2019-03-21 17:45:12 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
+const axios = require('axios');
+const _ = require('lodash');
 
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
@@ -8,5 +10,48 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
 
 app.get('/', (request, response) => {
-    
+    response.render('index.hbs', {
+        title_page: 'Official Front Page',
+        header: 'IT\'S TIME TO D-D-D-DUEL!'
+    })
+});
+
+app.get('/home', (request, response) => {
+    response.render('index.hbs', {
+        title_page: 'Official Front Page',
+        header: 'IT\'S TIME TO D-D-D-DUEL!'
+    })
+});
+
+app.get('/login', (request, response) => {
+    response.render('login.hbs', {
+        title_page: 'Login Page',
+        header: 'IT\'S TIME TO D-D-D-DUEL!'
+    })
+});
+
+app.get('/about', (request, response) => {
+    response.render('about.hbs', {
+        title_page: 'About Page',
+        header: 'IT\'S TIME TO D-D-D-DUEL!'
+    })
+});
+
+app.get('/character', (request, response) => {
+    response.render('character.hbs', {
+        title_page: 'My Character Page',
+        header: 'IT\'S TIME TO D-D-D-DUEL!'
+    })
+});
+
+
+app.get('/account', (request, response) => {
+    response.render('account.hbs', {
+        title_page: 'My Account Page',
+        header: 'IT\'S TIME TO D-D-D-DUEL!'
+    })
+});
+
+app.listen(8080, () => {
+    console.log('Server is up on the port 8080');
 });
