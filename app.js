97ec51294a8f2d5af256cde238fdb9f58f62eb32
Jimmy Truong <jtruong20@my.bcit.ca>
2019-04-09 10:59:34 -0700

    justin did the work

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var authentication = false;
 var user = 'Characters';
 
 const user_db = require('./javascript/user_db.js');
+const character_db = require('./javascript/character_db.js');
 
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
@@ -114,15 +115,117 @@ app.get('/character', (request, response) => {
     if (authentication === false) {
         response.redirect('/login')
     } else {
-        response.render('character.hbs', {
-            title_page: 'My Character Page',
-            header: 'Character Stats',
-            username: user,
-            character_name: 'test_name',
-            character_health: 'test_health',
-            character_dps: 'test_dps'
-        })
+        var db = character_db.getDb();
+        db.collection('Character').find({email: user}).toArray((err, item) => {
+            if (err) {
+                console.log(err)
+            } else {
+                try {
+                    var character_name = item[0].character_name;
+                    var health = item[0].health;
+                    var dps = item[0].dps;
+                    var win = item[0].win;
+                    var lose = item[0].lose;
+
+                    response.render('character.hbs', {
+                        title_page: 'My Character Page',
+                        header: 'Character Stats',
+                        username: user,
+                        character_name: `${character_name}`,
+                        character_health: `${health}`,
+                        character_dps: `${dps}`
+                    })
+                } catch (e) {
+                    response.render('character.hbs', {
+                        title_page: 'My Character Page',
+                        header: 'Character Stats',
+                        username: user,
+                        character_name: 'CREATE CHARACTER NOW',
+                        character_health: 'CREATE CHARACTER NOW',
+                        character_dps: 'CREATE CHARACTER NOW'
+                    })
+                }
+            }
+        });
     }
+});
+
+// const find = async (user) => {
+//     try {
+//         const db = character_db.getDb();
+//         const collection = db.collection('Character');
+//         const answer = await collection.findOne({email: user});
+//         return answer
+//     } catch (e) {
+//         console.log(e)
+//     }
+// };
+// console.log(find('95jvt71@gmail.com'));
+
+app.get('/character_creation', (request, response) => {
+    if (authentication === false) {
+        response.redirect('/login')
+    } else {
+        var db = character_db.getDb();
+
+        db.collection('Character').find({email: user}).toArray((err, items) => {
+            if (err) {
+                response.send(`alert('Cannot find it')`)
+            } else {
+                try {
+                    if (items[0].email === user) {
+                        output = "You already have a character ready for battle!";
+                        response.render('character_creation.hbs', {
+                            title_page: 'Character Creation',
+                            header: 'Create Character',
+                            username: user,
+                            output_error: `${output}`
+                        })
+                    }
+                } catch (e) {
+                    output = "Create a character now!";
+                    response.render('character_creation.hbs', {
+                        title_page: 'Character Creation',
+                        header: 'Create Character',
+                        username: user,
+                        output_error: `${output}`
+                    })
+                }
+            }
+        });
+    }
+});
+
+app.post('/create_character', (request, response) => {
+    var character_name = request.body.character_name;
+    var db = character_db.getDb();
+
+    db.collection('Character').find({email: user}).toArray( (err, item) => {
+        if (err) {
+            response.send('Unable to get all students')
+        } else {
+            try {
+                if(item[0].email === user) {
+                    response.redirect('/character_creation')
+                }
+            } catch (e) {
+                db.collection('Character').insertOne({
+                    character_name: character_name,
+                    email: user,
+                    health: 10,
+                    dps: 6,
+                    win: 0,
+                    lose: 0
+                }, (err, result) => {
+                    if (err) {
+                        response.send('Unable to insert stats');
+                    }
+                    // response.send(JSON.stringify(result.ops, undefined, 2));
+                    response.redirect('/character')
+                });
+            }
+        }
+    });
 });
 
 
@@ -142,4 +245,5 @@ app.get('/account', (request, response) => {
 
 app.listen(port, () => {
     console.log(`Server is up on the port ${port}`);
+    character_db.init();
 });
